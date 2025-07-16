9695e669faaaf41ae48aa1eec374588249b95ddb
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-07-16 16:50:58 +0100

    updated commit by engr. Ipaye

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,18 @@
-#Dockerfile
-FROM ubuntu:20.04
+FROM ubuntu:22.04
 
+# Avoid interactive prompt issues
+ENV DEBIAN_FRONTEND=noninteractive
+
+# Install SSH and Python
 RUN apt-get update && \
-    apt-get install -y openssh-server python3 && \
-    mkdir 'root:root' | chpasswd
+    apt-get install -y openssh-server python3 sudo && \
+    mkdir /var/run/sshd && \
+    echo 'root:root' | chpasswd && \
+    sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config && \
+    sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
+# Expose SSH port
 EXPOSE 22
+
+# Start SSH server
 CMD ["/usr/sbin/sshd", "-D"]
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,39 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.25.0"
+    }
+  }
+}
+
 provider "docker" {
-    host = "unix://var/run/docker.sock"
+  # Windows Docker Engine
+  host = "npipe:////./pipe/docker_engine"  # <-- use this on Windows
+
 }
 
 resource "docker_image" "ubuntu" {
-    name = "local-ubuntu-ssh"
-    build {
-        context = "../"
-        dockerfile = "Dockerfile"
-        }
+  name = "local-ubuntu-ssh"
+  build {
+    context    = "../"
+    dockerfile = "Dockerfile"
+  }
 }
 
 resource "docker_container" "webserver" {
-    name = "webserver"
-    image = docker_image.ubuntu.latest
-    ports {
-        internal = 22
-        external = 2222
-        }
+  name  = "webserver"
+  image = docker_image.ubuntu.image_id
 
-    provisioner "local-exec" {
-        command = "echo '[web]\nlocalhost ansible_ports=2222 ansible_host=127.0.0.1 ansible_user=root ansible_password=root ansible_connection=ssh' > ../ansible/inventory.ini"
-        }
-    }
+  ports {
+    internal = 22
+    external = 2222
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+echo "[web]
+localhost ansible_port=2222 ansible_host=127.0.0.1 ansible_user=root ansible_password=root ansible_connection=ssh" > ../ansible/inventory.ini
+EOT
+  }
+}
